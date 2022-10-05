075b17d8e12eb883362dc62ce7f9f451dcce25ee
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-10-04 23:51:37 -0400

    Add Actions build and deps.edn
    CLJ-2619: clear future thread bindings after execution
    
    flag
    
    wip
    
    build

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2036,27 +2036,67 @@
    :static true}
   [sym] (. clojure.lang.Var (find sym)))
 
+;if restore? is false (default), bindings will persist after returned
+;function finishes executing.
 (defn binding-conveyor-fn
   {:private true
    :added "1.3"}
-  [f]
-  (let [frame (clojure.lang.Var/cloneThreadBindingFrame)]
-    (fn 
-      ([]
-         (clojure.lang.Var/resetThreadBindingFrame frame)
-         (f))
-      ([x]
-         (clojure.lang.Var/resetThreadBindingFrame frame)
-         (f x))
-      ([x y]
-         (clojure.lang.Var/resetThreadBindingFrame frame)
-         (f x y))
-      ([x y z]
-         (clojure.lang.Var/resetThreadBindingFrame frame)
-         (f x y z))
-      ([x y z & args] 
-         (clojure.lang.Var/resetThreadBindingFrame frame)
-         (apply f x y z args)))))
+  ([f] (binding-conveyor-fn f false))
+  ([f restore?]
+   (let [frame (clojure.lang.Var/cloneThreadBindingFrame)]
+     (if restore?
+       (fn
+         ([]
+          (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+            (clojure.lang.Var/resetThreadBindingFrame frame)
+            (try
+              (f)
+              (finally
+                (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+         ([x]
+          (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+            (clojure.lang.Var/resetThreadBindingFrame frame)
+            (try
+              (f x)
+              (finally
+                (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+         ([x y]
+          (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+            (clojure.lang.Var/resetThreadBindingFrame frame)
+            (try
+              (f x y)
+              (finally
+                (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+         ([x y z]
+          (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+            (clojure.lang.Var/resetThreadBindingFrame frame)
+            (try
+              (f x y z)
+              (finally
+                (clojure.lang.Var/resetThreadBindingFrame previous-frame)))))
+         ([x y z & args] 
+          (let [previous-frame (clojure.lang.Var/getThreadBindingFrame)]
+            (clojure.lang.Var/resetThreadBindingFrame frame)
+            (try
+              (apply f x y z args)
+              (finally
+                (clojure.lang.Var/resetThreadBindingFrame previous-frame))))))
+       (fn 
+         ([]
+          (clojure.lang.Var/resetThreadBindingFrame frame)
+          (f))
+         ([x]
+          (clojure.lang.Var/resetThreadBindingFrame frame)
+          (f x))
+         ([x y]
+          (clojure.lang.Var/resetThreadBindingFrame frame)
+          (f x y))
+         ([x y z]
+          (clojure.lang.Var/resetThreadBindingFrame frame)
+          (f x y z))
+         ([x y z & args] 
+          (clojure.lang.Var/resetThreadBindingFrame frame)
+          (apply f x y z args)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Refs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn ^{:private true}
@@ -7036,6 +7076,7 @@ fails, attempts to require sym's namespace and retries."
     (.write w (str content))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; futures (needs proxy);;;;;;;;;;;;;;;;;;
+(def ^:private __futjure-CLJ-2619 (= "true" (System/getProperty "futjure.CLJ-2619")))
 (defn future-call 
   "Takes a function of no args and yields a future object that will
   invoke the function in another thread, and will cache the result and
@@ -7045,7 +7086,7 @@ fails, attempts to require sym's namespace and retries."
   {:added "1.1"
    :static true}
   [f]
-  (let [f (binding-conveyor-fn f)
+  (let [f (binding-conveyor-fn f __futjure-CLJ-2619)
         fut (.submit clojure.lang.Agent/soloExecutor ^Callable f)]
     (reify 
      clojure.lang.IDeref 
