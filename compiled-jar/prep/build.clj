cd46ec8ce0b34262e0cf24bef23acc7718dc999e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-11-03 12:15:49 -0400

    fix

--- compiled-jar/prep/build.clj
+++ compiled-jar/prep/build.clj
@@ -2,10 +2,11 @@
   (:require [clojure.java.shell :as sh]))
 
 (defn package-compiled-jar [_]
-  (let [{:keys [exit]} (sh/sh "cd .." "&&" "mvn" "package" "-Dmaven.test.skip=true")
-        _ (assert (zero? exit))]
-    (sh/sh "mkdir" "-p" "extracted-jar" "&&"
-           "cp" "../target/futjure-1.12.0-master-SNAPSHOT.jar" "extracted-jar" "&&"
-           "cd" "extracted-jar" "&&"
-           "jar" "xf" "futjure-1.12.0-master-SNAPSHOT.jar" "&&"
-           "rm" "futjure-1.12.0-master-SNAPSHOT.jar")))
+  (let [{:keys [exit]} (sh/sh "cd" ".." "&&" "mvn" "package" "-Dmaven.test.skip=true")
+        _ (assert (zero? exit))
+        {:keys [exit]} (sh/sh "mkdir" "-p" "extracted-jar" "&&"
+                              "cp" "../target/futjure-1.12.0-master-SNAPSHOT.jar" "extracted-jar" "&&"
+                              "cd" "extracted-jar" "&&"
+                              "jar" "xf" "futjure-1.12.0-master-SNAPSHOT.jar" "&&"
+                              "rm" "futjure-1.12.0-master-SNAPSHOT.jar")
+        _ (assert (zero? exit))]))
