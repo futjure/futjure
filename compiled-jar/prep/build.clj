d0ad9d883e5690447d947d19277117cf1fe3d80c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-11-03 12:23:26 -0400

    fix

--- compiled-jar/prep/build.clj
+++ compiled-jar/prep/build.clj
@@ -1,12 +1,16 @@
 (ns build
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh])
+  (:import [java.io File]))
+
+(defn sh! [& args]
+  (let [{:keys [exit]} (apply sh/sh args)]
+    (assert (zero? exit))))
 
 (defn package-compiled-jar [_]
-  (let [{:keys [exit]} (sh/sh "cd" ".." "&&" "mvn" "package" "-Dmaven.test.skip=true")
-        _ (assert (zero? exit))
-        {:keys [exit]} (sh/sh "mkdir" "-p" "extracted-jar" "&&"
-                              "cp" "../target/futjure-1.12.0-master-SNAPSHOT.jar" "extracted-jar" "&&"
-                              "cd" "extracted-jar" "&&"
-                              "jar" "xf" "futjure-1.12.0-master-SNAPSHOT.jar" "&&"
-                              "rm" "futjure-1.12.0-master-SNAPSHOT.jar")
-        _ (assert (zero? exit))]))
+  (sh! "mvn" "package" "-Dmaven.test.skip=true"
+       :dir "..")
+  (sh! "mkdir" "-p" "extracted-jar")
+  (sh! "cp" "../target/futjure-1.12.0-master-SNAPSHOT.jar" "extracted-jar")
+  (sh! "jar" "xf" "futjure-1.12.0-master-SNAPSHOT.jar"
+       :dir "extracted-jar")
+  (sh! "rm" "extracted-jar/futjure-1.12.0-master-SNAPSHOT.jar"))
