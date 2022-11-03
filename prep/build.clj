6f99eb6666bce61d5517bbf22c0f8da38b0fa058
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-11-02 22:07:40 -0400

    compile

--- prep/build.clj
+++ prep/build.clj
@@ -1,4 +1,5 @@
 (ns build
+  (:refer-clojure :exclude [compile])
   (:require [clojure.tools.build.api :as b]
             [clojure.tools.build.util.file :as file]
             [clojure.java.shell :as sh]))
@@ -6,6 +7,6 @@
 (def class-dir "target/classes")
 
 (defn compile [_]
-  (sh/sh "mvn clean compile")
+  (sh/sh "mvn" "clean" "compile")
   (b/copy-dir {:src-dirs [class-dir]
                :target-dir class-dir}))
