636ac4668f83b920e25beff172608a2d3bcbe643
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-11-03 12:10:24 -0400

    extracted

--- compiled-jar/prep/build.clj
+++ compiled-jar/prep/build.clj
@@ -1,9 +1,11 @@
 (ns build
   (:require [clojure.java.shell :as sh]))
 
-(defn package-all [_]
+(defn package-compiled-jar [_]
   (let [{:keys [exit]} (sh/sh "cd .." "&&" "mvn" "package" "-Dmaven.test.skip=true")
-        _ (assert (zero? exit))]))
-
-(def package-compiled-jar package-all)
-(def package-slim-jar package-all)
+        _ (assert (zero? exit))]
+    (sh/sh "mkdir" "-p" "extracted-jar" "&&"
+           "cp" "../target/futjure-1.12.0-master-SNAPSHOT.jar" "extracted-jar" "&&"
+           "cd" "extracted-jar" "&&"
+           "jar" "xf" "futjure-1.12.0-master-SNAPSHOT.jar" "&&"
+           "rm" "futjure-1.12.0-master-SNAPSHOT.jar")))
